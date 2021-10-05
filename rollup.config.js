6743f0043151c75d144066adb41370f3ba918eab
yujong-lee <yujonglee@g.skku.edu>
2021-10-05 17:08:24 +0900

    Fix build error

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import { terser } from 'rollup-plugin-terser';
 import typescript from '@rollup/plugin-typescript';
 
 export default {
-  input: 'src/index.ts',
+  input: '/index.ts',
   output: {
     format: 'iife',
     name: 'app',
