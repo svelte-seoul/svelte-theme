c744b67cd54995c8b94f504ee7caada941baeddd
yujong-lee <yujonglee@g.skku.edu>
2021-11-11 22:00:24 +0900

    Pass test

--- src/__tests__/theme.test.ts
+++ src/__tests__/theme.test.ts
@@ -1,9 +1,27 @@
-// @ts-ignore
-import SampleComponent from "../../test/SampleComponent.svelte";
 import {render} from "@testing-library/svelte";
 
-it("should render without error", () => {
-  const testngLib = render(SampleComponent);
+import SampleComponent from "../../test/SampleComponent.svelte";
 
-  expect(testngLib.container).toMatchSnapshot();
+describe('svelte-theme', () => {
+  beforeAll(() => {
+    Object.defineProperty(window, 'matchMedia', {
+      writable: true,
+      value: jest.fn().mockImplementation((query) => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(), // Deprecated
+        removeListener: jest.fn(), // Deprecated
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+      })),
+    });
+  });
+  
+  it("renders without error", () => {
+    const testngLib = render(SampleComponent);
+  
+    expect(testngLib.container).toMatchSnapshot();
+  });
 });
