311374b635d8b067190928a8455bd83726432d54
hyochan <dooboolab@gmail.com>
2022-04-03 20:19:59 +0900

    Publish 1.0.0 sveltekit-theme

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
     "^.+\\.(ts|tsx)$": "ts-jest",
     "^.+\\.svelte$": ["svelte-jester", { preprocess: true }],
   },
-  moduleNameMapper: { "^.+\\.(css|less|scss)$": "babel-jest" },
+  moduleNameMapper: { "^.+\\.(css|less|scss)$": "babel-jest", "^\\$app(.*)$": "<rootDir>/.svelte-kit/dev/runtime/app$1" },
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(js?|ts?|svelte?)$",
   moduleFileExtensions: ["js", "ts", "svelte"],
   modulePathIgnorePatterns: ["lib", "build"],
